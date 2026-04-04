a4c22f2974e11a858fd9424891e8e9ef343d7260
taohua656 <yan.zhenqi@163.com>
2026-04-04 19:09:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,5 +55,5 @@ async def generate_photo(
 if__name__=="__main__":
 import os
 port = int(os.environ.get("PORT",8000))
-print(f"端口：{port}"))
+printf("端口：{port}")
 uvicorn.run("main:app", host="0.0.0.0", port = port,reload=False)
