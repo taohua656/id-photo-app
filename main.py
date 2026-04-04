f1bf789d62ae2faab461c4d01b145439940e1517
taohua656 <yan.zhenqi@163.com>
2026-04-04 20:12:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def generate_photo(
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-import os
-port = int(os.environ.get("PORT",8000))
-printf("端口：{port}")
-uvicorn.run("main:app", host="0.0.0.0", port = port,reload=False)
+    import os
+    port = int(os.environ.get("PORT",8000))
+    printf("端口：{port}")
+    uvicorn.run("main:app", host="0.0.0.0", port = port,reload=False)
