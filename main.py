d84cccc639fd97c1d5a4ec0c1a80a7781fcebb4a
taohua656 <yan.zhenqi@163.com>
2026-04-04 17:23:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ async def generate_photo(
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT",8000))
-    uvicorn.run(main:app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
