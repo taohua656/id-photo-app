38f9a64c531c6f72b54b27b9deb6a2df7625c7a1
taohua656 <yan.zhenqi@163.com>
2026-04-07 06:23:18 +0800

    fix main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // 确保这里指向你的主组件
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
